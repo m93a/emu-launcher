65f8e4433ec3a09cceef8a5f70415c78a1182654
m93a <m93a.cz@gmail.com>
2022-08-29 09:50:31 +0200

    migrate to threlte

--- src/lib/DiscSelection.svelte
+++ src/lib/DiscSelection.svelte
@@ -1,22 +1,17 @@
 <script lang="ts">
-  import {
-    Canvas,
-    Mesh,
-    PerspectiveCamera,
-    AmbientLight,
-    DirectionalLight,
-  } from "svelte-cubed";
-  import { BoxGeometry, MeshStandardMaterial, Color } from "three";
+  import { Canvas, Mesh, PerspectiveCamera, AmbientLight, DirectionalLight } from "@threlte/core";
+  import { Text } from "@threlte/extras";
+  import { BoxBufferGeometry, MeshStandardMaterial, Color } from "three";
+  const DEG = Math.PI / 180;
 </script>
 
-<Canvas background={new Color("gray")}>
+<Canvas>
   <Mesh
-    geometry={new BoxGeometry()}
+    geometry={new BoxBufferGeometry(1, 1, .1)}
     material={new MeshStandardMaterial({ color: 0xff3e00 })}
-    scale={[1, 1, 1]}
-    rotation={[0, 0, 0]}
+    rotation={{ y: 45 * DEG }}
   />
-  <PerspectiveCamera position={[1, 1, 3]} />
+  <PerspectiveCamera position={{ x: 1, y: 0, z: 3 }} />
   <AmbientLight intensity={0.6} />
-  <DirectionalLight intensity={0.6} position={[-2, 3, 2]} />
+  <DirectionalLight intensity={0.6} position={{ x: -2, y: 3, z: 2 }} />
 </Canvas>
